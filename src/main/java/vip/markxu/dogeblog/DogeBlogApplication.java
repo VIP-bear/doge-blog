73e2e20bc65d83a924ed6a9388ca8df5fcfedb78
markxu <xubiaolin@markxu.vip>
2020-03-07 19:02:12 +0800

    Initial commit

---
+++ src/main/java/vip/markxu/dogeblog/DogeBlogApplication.java
@@ -0,0 +1,13 @@
+package vip.markxu.dogeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogeBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogeBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vip/markxu/dogeblog/DogeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.markxu.dogeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogeBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
